d9e7cb310052a50c03e70554334be0eb187e53e2
Edward Capriolo <edlinuxguru@gmail.com>
2014-11-28 20:38:20 -0500

    m5d partitioner

---
+++ src/main/java/Md5Partitioner.java
@@ -0,0 +1,33 @@
+import java.security.NoSuchAlgorithmException;
+
+
+public class Md5Partitioner implements Partitioner {
+
+  private java.security.MessageDigest md; 
+          
+  public Md5Partitioner(){
+    try {
+      md = java.security.MessageDigest.getInstance("MD5");
+    } catch (NoSuchAlgorithmException e) {
+      throw new RuntimeException("DIE!");
+    }
+  } 
+  
+  public String MD5(String md5) {
+    byte[] array = md.digest(md5.getBytes());
+    StringBuffer sb = new StringBuffer();
+    for (int i = 0; i < array.length; ++i) {
+      sb.append(Integer.toHexString((array[i] & 0xFF) | 0x100).substring(1, 3));
+    }
+    return sb.toString();
+  }
+  
+  @Override
+  public Token partition(String in) {
+    Token t = new Token();
+    t.setRowkey(in);
+    t.setToken(MD5(in));
+    return t;
+  }
+
+}
---
+++ src/test/java/PartitionerTest.java
@@ -0,0 +1,12 @@
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+public class PartitionerTest {
+
+  @Test
+  public void test(){
+    Md5Partitioner p = new Md5Partitioner();
+    Assert.assertEquals("49f68a5c8493ec2c0bf489821c21fc3b", p.partition("hi").getToken());
+  }
+}
