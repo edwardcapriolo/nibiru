e8b71d500d3565bb6f8099ecbecd87d812b3ed24
Edward Capriolo <edlinuxguru@gmail.com>
2014-12-30 09:32:04 -0500

    Class for reading commit logs

---
+++ src/main/java/io/teknek/nibiru/engine/CommitLogReader.java
@@ -0,0 +1,56 @@
+package io.teknek.nibiru.engine;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.util.SortedMap;
+
+public class CommitLogReader {
+
+  private RandomAccessFile ssRaf;
+  private FileChannel ssChannel;
+  private MappedByteBuffer ssBuffer;
+  private BufferGroup bg;
+  private final String id;
+  private final ColumnFamily columnFamily;
+  
+  public CommitLogReader(String id, ColumnFamily columnFamily){
+    this.id = id;
+    this.columnFamily = columnFamily;
+  }
+  
+  public void open() throws IOException {
+    File sstable = new File(columnFamily.getKeyspace().getConfiguration().getSstableDirectory(), id + CommitLog.EXTENSION);
+    ssRaf = new RandomAccessFile(sstable, "r");
+    ssChannel = ssRaf.getChannel();
+    ssBuffer = ssChannel.map(FileChannel.MapMode.READ_ONLY, 0, ssChannel.size());
+    bg.channel = ssChannel; 
+    bg.mbb = (MappedByteBuffer) ssBuffer.duplicate();
+    bg.setStartOffset((int) 0);
+  }
+  
+  public Token getNextToken() throws IOException {
+    if (! (bg.currentIndex < bg.dst.length - 1 || bg.mbb.position()  < bg.channel.size())){
+      return null;
+    }
+    if (bg.dst[bg.currentIndex] == SsTableReader.END_ROW){
+      bg.advanceIndex();
+    }
+    SsTableReader.readHeader(bg); 
+    StringBuilder token = SsTableReader.readToken(bg);
+    StringBuilder rowkey = SsTableReader.readRowkey(bg);
+    Token t = new Token();
+    t.setRowkey(rowkey.toString());
+    t.setToken(token.toString());
+    return t;
+  }
+  
+  public SortedMap<String,Val>  readColumns() throws IOException {
+    SortedMap<String,Val> columns = SsTableReader.readColumns(bg);
+    return columns;
+  }
+  
+}
