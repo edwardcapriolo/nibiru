4c07cefde2c59c70fdc5801b616db7de8319109e
Edward Capriolo <edlinuxguru@gmail.com>
2015-01-04 12:22:14 -0500

    Adding personalities

--- src/main/java/io/teknek/nibiru/ColumnFamily.java
+++ src/main/java/io/teknek/nibiru/ColumnFamily.java
@@ -25,11 +25,4 @@ public abstract class ColumnFamily {
 
   public abstract void shutdown() throws IOException;
 
-  public abstract Val get(String rowkey, String column);
-
-  public abstract void delete(String rowkey, String column, long time);
-
-  public abstract void put(String rowkey, String column, String value, long time, long ttl);
-
-  public abstract void put(String rowkey, String column, String value, long time);
 }
---
+++ src/main/java/io/teknek/nibiru/Message.java
@@ -0,0 +1,49 @@
+package io.teknek.nibiru;
+
+import java.util.Map;
+
+public class Message {
+  private String keyspace;
+  private String columnFamily;
+  private String requestPersonality;
+
+  private Map<String,Object> payload;
+  
+  public Message(){
+    
+  }
+  
+  public String getKeyspace() {
+    return keyspace;
+  }
+  
+  public void setKeyspace(String keyspace) {
+    this.keyspace = keyspace;
+  }
+  
+  public String getColumnFamily() {
+    return columnFamily;
+  }
+  
+  public void setColumnFamily(String columnFamily) {
+    this.columnFamily = columnFamily;
+  }
+  
+  public Map<String, Object> getPayload() {
+    return payload;
+  }
+  
+  public void setPayload(Map<String, Object> payload) {
+    this.payload = payload;
+  }
+
+  public String getRequestPersonality() {
+    return requestPersonality;
+  }
+
+  public void setRequestPersonality(String requestPersonality) {
+    this.requestPersonality = requestPersonality;
+  }
+
+  
+}
