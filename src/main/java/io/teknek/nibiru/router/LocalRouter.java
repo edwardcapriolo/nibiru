7d0df49812ba2c1a75579da777d46b52f6896527
Edward Capriolo <edlinuxguru@gmail.com>
2015-01-15 19:20:20 -0500

    Format

--- src/main/java/io/teknek/nibiru/router/LocalRouter.java
+++ src/main/java/io/teknek/nibiru/router/LocalRouter.java
@@ -13,7 +13,8 @@ import java.util.List;
 public class LocalRouter implements Router{
 
   @Override
-  public List<Destination> routesTo(Message message, ServerId local, Keyspace requestKeyspace, ClusterMembership clusterMembership, Token token) {
+  public List<Destination> routesTo(Message message, ServerId local, Keyspace requestKeyspace,
+          ClusterMembership clusterMembership, Token token) {
     Destination d = new Destination();
     d.setDestinationId(local.getU().toString());
     return Arrays.asList(d);
