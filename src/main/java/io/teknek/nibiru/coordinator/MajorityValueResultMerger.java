7dfdbf86bede67e81322ef04865ebf05ad72080f
Edward Capriolo <edlinuxguru@gmail.com>
2015-01-31 20:13:22 -0500

    Show the merger and what it does

--- src/main/java/io/teknek/nibiru/coordinator/MajorityValueResultMerger.java
+++ src/main/java/io/teknek/nibiru/coordinator/MajorityValueResultMerger.java
@@ -3,15 +3,16 @@ package io.teknek.nibiru.coordinator;
 import io.teknek.nibiru.transport.Message;
 import io.teknek.nibiru.transport.Response;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.SortedMap;
+import java.util.TreeMap;
 
 public class MajorityValueResultMerger implements ResultMerger {
 
   @Override
   public Response merge(List<Response> responses, Message message) {
-    Map<String,Integer> count = new HashMap<String,Integer>();
+    SortedMap<String,Integer> count = new TreeMap<String,Integer>();
     for (Response r : responses){
       String result = (String) r.get("payload");
       if (count.containsKey(result)){
---
+++ src/test/java/io/teknek/nibiru/keyvalue/TestMajorityMerger.java
@@ -0,0 +1,52 @@
+package io.teknek.nibiru.keyvalue;
+
+import java.util.Arrays;
+
+import junit.framework.Assert;
+import io.teknek.nibiru.coordinator.MajorityValueResultMerger;
+import io.teknek.nibiru.transport.Response;
+
+import org.junit.Test;
+
+public class TestMajorityMerger {
+
+  @Test
+  public void majority(){
+    MajorityValueResultMerger mm = new MajorityValueResultMerger();
+    Assert.assertEquals("a", mm.merge(Arrays.asList( 
+            new Response().withProperty("payload", "a"),
+            new Response().withProperty("payload", "b"),
+            new Response().withProperty("payload", "a")
+            ), null).get("payload"));    
+  }
+  
+  @Test
+  public void anotherMajority(){
+    MajorityValueResultMerger mm = new MajorityValueResultMerger();
+    Assert.assertEquals("b", mm.merge(Arrays.asList( 
+            new Response().withProperty("payload", "a"),
+            new Response().withProperty("payload", "b"),
+            new Response().withProperty("payload", "b")
+            ), null).get("payload"));    
+  }
+  
+  @Test
+  public void aPlurality(){
+    MajorityValueResultMerger mm = new MajorityValueResultMerger();
+    Assert.assertEquals("a", mm.merge(Arrays.asList( 
+            new Response().withProperty("payload", "a"),
+            new Response().withProperty("payload", "a"),
+            new Response().withProperty("payload", "b"),
+            new Response().withProperty("payload", "b")
+            ), null).get("payload"));    
+    
+    Assert.assertEquals("a", mm.merge(Arrays.asList( 
+            new Response().withProperty("payload", "b"),
+            new Response().withProperty("payload", "b"),
+            new Response().withProperty("payload", "a"),
+            new Response().withProperty("payload", "a")
+            ), null).get("payload")); 
+    
+  }
+ 
+}
