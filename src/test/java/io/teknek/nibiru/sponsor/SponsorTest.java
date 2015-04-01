b694ae1525a5427f4ee0c4f2ce708b1b1f80d0a6
Edward Capriolo <edlinuxguru@gmail.com>
2015-04-01 19:05:15 -0400

    Somewhat cleaner

--- src/test/java/io/teknek/nibiru/sponsor/SponsorTest.java
+++ src/test/java/io/teknek/nibiru/sponsor/SponsorTest.java
@@ -13,6 +13,7 @@ import io.teknek.nibiru.engine.atom.ColumnValue;
 import io.teknek.nibiru.metadata.KeyspaceMetaData;
 import io.teknek.nibiru.metadata.StoreMetaData;
 import io.teknek.nibiru.router.TokenRouter;
+import io.teknek.nibiru.transport.Response;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -32,35 +33,26 @@ public class SponsorTest {
   @Rule
   public TemporaryFolder node2Folder = new TemporaryFolder();
   
+
   @Test
   public void test() throws ClientException, InterruptedException{
-    Server[] s = new Server[2];
-    TemporaryFolder [] t = { node1Folder, node2Folder};
+    Server[] servers = new Server[2];
+    TemporaryFolder [] tempFolders = { node1Folder, node2Folder};
     Configuration [] cs = new Configuration[2];
     for (int i = 0; i < cs.length; i++) {
-      cs[i] = TestUtil.aBasicConfiguration(t[i]);
+      cs[i] = TestUtil.aBasicConfiguration(tempFolders[i]);
       cs[i].setTransportHost("127.0.0." + (i + 1));
       cs[i].setClusterMembershipProperties(TestUtil.gossipPropertiesFor127Seed());
-      s[i] = new Server(cs[i]);
+      servers[i] = new Server(cs[i]);
     }
-    s[0].init();
+    servers[0].init();
         
-    MetaDataClient metaClient = new MetaDataClient(s[0].getConfiguration().getTransportHost(), s[0]
+    MetaDataClient metaClient = new MetaDataClient(servers[0].getConfiguration().getTransportHost(), servers[0]
             .getConfiguration().getTransportPort());
-    Map<String,Object> props = new HashMap<>();
-    TreeMap<String,String> tokenMap = new TreeMap<>();
-    tokenMap.put("10", s[0].getServerId().getU().toString());
-    props.put(TokenRouter.TOKEN_MAP_KEY, tokenMap);
-    props.put(TokenRouter.REPLICATION_FACTOR, 1);
-    props.put(KeyspaceMetaData.ROUTER_CLASS, TokenRouter.class.getName());
-    metaClient.createOrUpdateKeyspace("abc", props, true);
+    createKeyspaceInformation(metaClient, servers);    
+    Assert.assertEquals(servers[0].getClusterMembership().getLiveMembers().size(), 0);//We do not count ourselves
     
-    Map <String,Object> x = new HashMap<String,Object>();
-    x.put(StoreMetaData.IMPLEMENTING_CLASS, DefaultColumnFamily.class.getName());
-    metaClient.createOrUpdateStore("abc", "def", x);
-    Assert.assertEquals(s[0].getClusterMembership().getLiveMembers().size(), 0);//We do not count ourselves
-    
-    ColumnFamilyClient c = new ColumnFamilyClient(s[0].getConfiguration().getTransportHost(), s[0]
+    ColumnFamilyClient c = new ColumnFamilyClient(servers[0].getConfiguration().getTransportHost(), servers[0]
             .getConfiguration().getTransportPort());
     Session session = c.createBuilder().withKeyspace("abc")
             .withWriteConsistency(ConsistencyLevel.ALL, new HashMap())
@@ -69,36 +61,53 @@ public class SponsorTest {
     for (int k = 0; k < 10; k++) {
       session.put(k+"", k+"", k+"", 1);
     }
-    Assert.assertEquals(10, ((DefaultColumnFamily) s[0].getKeyspaces().get("abc").getStores().get("def")).getMemtable().size());
-    System.out.println("starting second node");
-    s[1].init(); 
     
+    servers[1].init(); 
     Thread.sleep(10000);
-    Assert.assertEquals(s[0].getClusterMembership().getLiveMembers().size(), 1);
-    System.out.println("joining second node");
-    s[1].join("abc", "127.0.0.1", "5");
+    Assert.assertEquals(servers[0].getClusterMembership().getLiveMembers().size(), 1);
+    
+    servers[1].join("abc", "127.0.0.1", "5");
     Thread.sleep(1000);
-    Assert.assertEquals(s[1].getServerId().getU().toString(), 
-            s[0].getCoordinator().getSponsorCoordinator().getProtege().getDestinationId());
+    Assert.assertEquals(servers[1].getServerId().getU().toString(), 
+            servers[0].getCoordinator().getSponsorCoordinator().getProtege().getDestinationId());
     
-    
-    
-    session.put("1", "1", "after", 8);
-    session.put("7", "7", "after", 8);
-    session.put("11", "11", "after", 8);
-    
-    Assert.assertEquals("after", ((ColumnValue) s[1].get("abc", "def", "11" , "11")).getValue());
-    Assert.assertEquals("after", ((ColumnValue) s[0].get("abc", "def", "1" , "1")).getValue());
-    
-    Assert.assertEquals("after", ((ColumnValue) s[1].get("abc", "def", "11" , "11")).getValue());
-    Assert.assertEquals("after", ((ColumnValue) s[1].get("abc", "def", "1" , "1")).getValue());
+    insertDataOverClient(session);
+    assertDataIsDistributed(servers);
     
     Thread.sleep(5000);
     Map<String,String> keyspaceMembers = (Map<String, String>) metaClient.getKeyspaceMetadata("abc").get(TokenRouter.TOKEN_MAP_KEY);
     Assert.assertEquals(2, keyspaceMembers.size());
+    
     for (int i = 0; i < cs.length; i++) {
-      s[i].shutdown();
+      servers[i].shutdown();
     }
     metaClient.shutdown();
   }
+
+  
+  private void insertDataOverClient(Session session) throws ClientException {
+    session.put("1", "1", "after", 8);
+    session.put("7", "7", "after", 8);
+    session.put("11", "11", "after", 8);
+  }
+  
+  private void assertDataIsDistributed(Server [] servers){
+    Assert.assertEquals("after", ((ColumnValue) servers[1].get("abc", "def", "11" , "11")).getValue());
+    Assert.assertEquals("after", ((ColumnValue) servers[0].get("abc", "def", "1" , "1")).getValue());
+    Assert.assertEquals("after", ((ColumnValue) servers[1].get("abc", "def", "11" , "11")).getValue());
+    Assert.assertEquals("after", ((ColumnValue) servers[1].get("abc", "def", "1" , "1")).getValue());
+  }
+  
+  private void createKeyspaceInformation(MetaDataClient metaClient, Server [] s) throws ClientException{
+    Map<String,Object> props = new HashMap<>();
+    TreeMap<String,String> tokenMap = new TreeMap<>();
+    tokenMap.put("10", s[0].getServerId().getU().toString());
+    props.put(TokenRouter.TOKEN_MAP_KEY, tokenMap);
+    props.put(TokenRouter.REPLICATION_FACTOR, 1);
+    props.put(KeyspaceMetaData.ROUTER_CLASS, TokenRouter.class.getName());
+    metaClient.createOrUpdateKeyspace("abc", props, true);
+    metaClient.createOrUpdateStore("abc", "def", new Response()
+    .withProperty(StoreMetaData.IMPLEMENTING_CLASS, DefaultColumnFamily.class.getName()));
+  }
+  
 }
